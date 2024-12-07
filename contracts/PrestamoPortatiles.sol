fbd4ddbcaf477dacfdaa40ef7ab34e011b3c4e43
Paula <paulacruz@iesgregorioprieto.com>
2024-12-07 23:53:39 +0100

    constructor del smart contract

--- contracts/PrestamoPortatiles.sol
+++ contracts/PrestamoPortatiles.sol
@@ -13,4 +13,13 @@ contract PrestamoPortatiles{
     }
 
     mapping (uint256 => Portatil) public portatiles;
+
+    constructor () {
+        owner = msg.sender; //la persona que está desplegando el smart contract en la blockchain
+        portatiles[0] = Portatil("Lenovo TIC01", Estado.Disponible, address(0));
+        portatiles[1] = Portatil("HP TIC02", Estado.Disponible, address(0));
+        portatiles[2] = Portatil("Lenovo TIC03", Estado.Disponible, address(0));
+        portatiles[3] = Portatil("TTL TIC04", Estado.Disponible, address(0));
+        portatiles[4] = Portatil("TTLTIC05", Estado.Disponible, address(0));
+    }
 }
