7eb9dc964b5dd55f483380cb2693933fefc69030
Paula <paulacruz@iesgregorioprieto.com>
2024-12-07 22:20:48 +0100

    rama main

--- contracts/PrestamoPortatiles.sol
+++ contracts/PrestamoPortatiles.sol
@@ -2,5 +2,15 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract PrestamoPortatiles{
-    
+    address public owner;
+
+    enum Estado {Disponible, Prestado}
+
+    struct Portatil{
+        string modeloPortatil;
+        Estado estadoPortatil;
+        address prestadoA;
+    }
+
+    mapping (uint256 => Portatil) public portatiles;
 }
